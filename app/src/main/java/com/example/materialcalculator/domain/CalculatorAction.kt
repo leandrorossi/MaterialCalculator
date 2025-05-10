4bab0fd91334f83668d280dd80c07478a4f38043
Leandro <le_andro18@hotmail.com>
2025-05-10 19:49:17 -0300

    feat: Implement expression writer

---
+++ app/src/main/java/com/example/materialcalculator/domain/CalculatorAction.kt
@@ -0,0 +1,11 @@
+package com.example.materialcalculator.domain
+
+sealed interface CalculatorAction {
+    data class Number(val number: Int): CalculatorAction
+    data class Op(val operation: Operation): CalculatorAction
+    data object Clear: CalculatorAction
+    data object Delete: CalculatorAction
+    data object Decimal: CalculatorAction
+    data object Calculate: CalculatorAction
+    data object Parentheses: CalculatorAction
+}
